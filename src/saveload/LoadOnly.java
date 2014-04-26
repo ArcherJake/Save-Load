4242ac0d97c6a919f328d15c57accc0111ef9247
ArcherJake <ArcherJake@gmail.com>
2014-04-26 11:02:50 -0400

    More testing

--- src/saveload/LoadOnly.java
+++ src/saveload/LoadOnly.java
@@ -20,8 +20,8 @@ public class LoadOnly {
 
 	private static ArrayList<ClassObject> classObjectList = new ArrayList<ClassObject>();
 	private static ArrayList<Relationship> relationList = new ArrayList<Relationship>();
-	private static ArrayList<ClassObject> classObjectList2 = new ArrayList<ClassObject>();	
-	private static ArrayList<Relationship> relationList2 = new ArrayList<Relationship>();	
+	private static ArrayList<ClassObject> classObjectList2 = new ArrayList<ClassObject>();
+	private static ArrayList<Relationship> relationList2 = new ArrayList<Relationship>();
 	private static String path = "UML.ser";
 
 	public static void main(String[] args) throws FileNotFoundException,
@@ -53,73 +53,76 @@ public class LoadOnly {
 
 		relationList.add(line);
 		relationList.add(line2);
-		
+
 		classObjectList.add(target1);
 		classObjectList.add(target2);
 
 		// SaveState();
 		System.out.println("Attempting Load:");
 		LoadState();
-		
+
 		LoadTest();
-		
-		
+
 	}
 
 	private static void LoadTest() {
 		/**
 		 * Checks all the information from a loaded class.
 		 */
-		
+
 		System.out.println("Start Test");
 		System.out.println("Checking sizes: " + classObjectList.size() + " vs "
 				+ classObjectList2.size());
-		
-		
+
 		for (int i = 0; i < classObjectList.size(); i++) {
-			int index = i+1;
-			System.out.println("Testing Classobject " + index + " of " + classObjectList.size());
+			int index = i + 1;
+			System.out.println("Testing Classobject " + index + " of "
+					+ classObjectList.size());
 			ClassObject held = classObjectList.get(i);
 			ClassObject loaded = classObjectList2.get(i);
-			//ArrayList<Attribute> heldA, loadedA;
-			//ArrayList<Operation> heldO, loadedO;
-			//name check first
+			// ArrayList<Attribute> heldA, loadedA;
+			// ArrayList<Operation> heldO, loadedO;
+			// name check first
 
 			System.out.println("Name Test: " + held.getName() + " vs "
 					+ loaded.getName());
-			
-			//check Attributes and Operations lists
-			System.out.println("Attributes List Size Test: " + held.getAttributes().size()
-					+ " vs " + loaded.getAttributes().size());
-			//heldA = held.getAttributes();
-			//loadedA = loaded.getAttributes();
-			for (int j = 0; j < held.getAttributes().size(); j++){			
-				
+
+			// check Attributes and Operations lists
+			System.out.println("Attributes List Size Test: "
+					+ held.getAttributes().size() + " vs "
+					+ loaded.getAttributes().size());
+			// heldA = held.getAttributes();
+			// loadedA = loaded.getAttributes();
+			for (int j = 0; j < held.getAttributes().size(); j++) {
+				System.out.println(held.getAttributes().get(j)
+						.getAttributeName()
+						+ " vs "
+						+ loaded.getAttributes().get(j).getAttributeName());
 			}
-			
-			System.out.println("Operations List Size Test: " + held.getOperations().size()
-					+ " vs " + loaded.getOperations().size());
-			
-			for (int j = 0; j < held.getOperations().size(); j++){
-			
-				
-				System.out.println(held.getOperations().get(j).getOperationName() + " vs " + loaded.getOperations().get(j).getOperationName());
+
+			System.out.println("Operations List Size Test: "
+					+ held.getOperations().size() + " vs "
+					+ loaded.getOperations().size());
+
+			for (int j = 0; j < held.getOperations().size(); j++) {
+
+				System.out.println(held.getOperations().get(j)
+						.getOperationName()
+						+ " vs "
+						+ loaded.getOperations().get(j).getOperationName());
 			}
-			
+
 			System.out.println();
 		}
-		
-		
+
 		for (int i = 0; i < relationList.size(); i++) {
 
-			System.out.println(relationList.get(i).getOrigin().getName() +
-					" to " + relationList.get(i).getDestination().getName()
-					+ " vs "	
-					+ relationList2.get(i).getOrigin().getName() + 
-					" to " + relationList2.get(i).getDestination().getName());
+			System.out.println(relationList.get(i).getOrigin().getName()
+					+ " to " + relationList.get(i).getDestination().getName()
+					+ " vs " + relationList2.get(i).getOrigin().getName()
+					+ " to " + relationList2.get(i).getDestination().getName());
 		}
 
-		
 	}
 
 	private static void LoadState() throws FileNotFoundException, IOException,
